e5b3803eb684f920407c000e57532f89a0b4d00d
kartik1411 <kartikvarshney1411@gmail.com>
2021-09-07 15:53:38 +0530

    header section is done

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,14 @@
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
 	return (
-		<div>
-		
-		</div>
+		<>
+			<Header />
+			<main>
+				<Meals />
+			</main>
+		</>
 	);
 }
 
---
+++ app/src/components/Layout/HeaderButtonCart.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderButtonCart.module.css';
+
+function HeaderButtonCart(props) {
+    return (
+        <button className={classes.button}>
+            <span className={classes.icon}>
+                <CartIcon />
+            </span>
+            <span>Cart</span>
+            <span className={classes.badge}>
+                3
+            </span>
+        </button>
+    )
+}
+
+export default HeaderButtonCart;
