50a3cefddd46cb4c062d6a676dd337544b304f1e
kartik1411 <kartikvarshney1411@gmail.com>
2021-09-15 16:46:40 +0530

    project done with firebase API

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
 
 function App() {
-
-	const[cartIsShown, setCartIsShown] = useState(false);
+	const [cartIsShown, setCartIsShown] = useState(false);
 
 	const showCartHandler = () => {
 		setCartIsShown(true);
-	}
+	};
 
 	const hideCartHandler = () => {
 		setCartIsShown(false);
-	}
+	};
 
 	return (
 		<CartProvider>
-			{cartIsShown && <Cart onHideCart={hideCartHandler}/>}
+			{cartIsShown && <Cart onClose={hideCartHandler} />}
 			<Header onShowCart={showCartHandler} />
 			<main>
 				<Meals />
